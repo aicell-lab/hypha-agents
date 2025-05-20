5a432b0788e5f9bd1e97fd13746f801522071de4
Wei Ouyang <oeway007@gmail.com>
2025-05-20 15:43:44 +0200

    Fix artifact listing

--- src/components/MyArtifacts.tsx
+++ src/components/MyArtifacts.tsx
@@ -48,12 +48,12 @@ const MyArtifacts: React.FC = () => {
       setLoading(true);
       const filters = {
         created_by: user.id,
-        version: showStagedOnly ? "stage" : "*",
       };
 
       const response = await artifactManager.list({
         parent_id: "hypha-agents/agents",
         filters: filters,
+        stage: showStagedOnly ? true : "all",
         limit: 100,
         _rkwargs: true
       });
